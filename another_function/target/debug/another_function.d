c10ad0256e165a03372d280f61dd01665f8cf3c1
snumigol <95432062+snumigol@users.noreply.github.com>
2023-05-18 08:10:10 +0100

    Add files via upload

---
+++ another_function/target/debug/another_function.d
@@ -0,0 +1,1 @@
+C:\Users\Serhii.Yahotin\Desktop\projects\another_function\target\debug\another_function.exe: C:\Users\Serhii.Yahotin\Desktop\projects\another_function\src\main.rs
---
+++ another_function/target/debug/deps/another_function.d
@@ -0,0 +1,5 @@
+C:\Users\Serhii.Yahotin\Desktop\projects\another_function\target\debug\deps\another_function.exe: src\main.rs
+
+C:\Users\Serhii.Yahotin\Desktop\projects\another_function\target\debug\deps\another_function.d: src\main.rs
+
+src\main.rs:
