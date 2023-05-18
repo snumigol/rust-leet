183d3c1f320280231e9d46680a54eb1c2c5832dc
snumigol <95432062+snumigol@users.noreply.github.com>
2023-05-18 08:12:15 +0100

    Add files via upload

---
+++ proj2/target/debug/deps/proj2.d
@@ -0,0 +1,5 @@
+C:\Users\Serhii.Yahotin\Desktop\rust\proj2\target\debug\deps\proj2.exe: src\main.rs
+
+C:\Users\Serhii.Yahotin\Desktop\rust\proj2\target\debug\deps\proj2.d: src\main.rs
+
+src\main.rs:
---
+++ proj2/target/debug/proj2.d
@@ -0,0 +1,1 @@
+C:\Users\Serhii.Yahotin\Desktop\rust\proj2\target\debug\proj2.exe: C:\Users\Serhii.Yahotin\Desktop\rust\proj2\src\main.rs
