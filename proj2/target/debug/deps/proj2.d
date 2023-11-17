309cd6cdb77ba7a5f1b9712f6387b7c0f185b839
snumigol <95432062+snumigol@users.noreply.github.com>
2023-11-17 15:21:10 +0000

    Delete proj2 directory

--- proj2/target/debug/deps/proj2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Serhii.Yahotin\Desktop\rust\proj2\target\debug\deps\proj2.exe: src\main.rs
-
-C:\Users\Serhii.Yahotin\Desktop\rust\proj2\target\debug\deps\proj2.d: src\main.rs
-
-src\main.rs:
--- proj2/target/debug/proj2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Serhii.Yahotin\Desktop\rust\proj2\target\debug\proj2.exe: C:\Users\Serhii.Yahotin\Desktop\rust\proj2\src\main.rs
